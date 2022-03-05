5b26f53d5ab0987f70b24ceb33fa5436425a87ec
a-calkins <81609703+a-calkins@users.noreply.github.com>
2022-03-04 21:53:28 -0800

    Add files via upload

--- Queries.cpp
+++ Queries.cpp
@@ -1,25 +1,49 @@
 #include "Queries.h"
-# include <vector>
+#include <vector>
 #include <string>
 #include <iostream>
+
 using namespace std;
 
+//prints the queries
+void Queries::ProcessQuery(PGconn* conn, const char* query) {
+	PGresult* res;
+	PQprintOpt opt = { 0 };
+
+	res = PQexec(conn, query);
+	if (res == NULL) {
+		cout << PQerrorMessage(conn) << endl;
+		return;
+	}
+	opt.header = 1; //gets column headers
+	opt.align = 1;
+	char fieldSep = ' ';
+	opt.fieldSep = &fieldSep;
+
+	//PQprint(stdout, res, &opt);
+	cout << PQgetvalue(res, 0, 0) << endl;
+	PQclear(res);
+}
 void Queries::GetGamesByUser(string email, PGconn* conn)
 {
+	/*char fieldSep = ' ';
+	char tableOpt = 't\0';
+	char caption = 't\0';
     PQprintOpt printOPT = {0};
     printOPT.header = '1';
     printOPT.align = '1';
-   printOPT.fieldSep = new char;
-    *printOPT.fieldSep = 't\0';
-    printOPT.tableOpt = new char;
-    *printOPT.tableOpt = 't\0';
-    printOPT.caption = new char;
-    *printOPT.caption = 't\0'; /*
+   printOPT.fieldSep = &fieldSep;
+    //*printOPT.fieldSep = 't\0';
+    //printOPT.tableOpt = &tableOpt;
+    //*printOPT.tableOpt = 't\0';
+    //printOPT.caption = &caption;
+    //*printOPT.caption = 't\0'; 
+	/*
     char* test = new char;
     *test = 't\0';
     char** testTwo = &test;
     printOPT.fieldName = testTwo;*/
-    PGresult* res = PQexec(conn, "BEGIN");
+    /*PGresult* res;
     //PQclear(res);
     string query = "SELECT Title FROM Game;"  ; // JOIN PurchaseItem ON(Game.ID = GameId)";
    // query += " JOIN Purchase ON (Purchase.id = PurchaseID) JOIN Account ON (AccountID = Account.id) ";
@@ -30,8 +54,12 @@ void Queries::GetGamesByUser(string email, PGconn* conn)
      //cout << *PQresultErrorMessage(res);
      PQclear(res);
      //PQfinish(conn);
-     ;
-     //delete res;
+     
+     //delete res;*/
+	string query = "SELECT Title FROM Game";
+	ProcessQuery(conn, query.c_str());
+
+
 }
 
 vector<string> Queries::GetGamesSold(PGconn* conn)
--- Queries.h
+++ Queries.h
@@ -2,11 +2,16 @@
 #include <string>
 #include <vector>
 #include <libpq-fe.h>
+
 //#include "Queries.cpp"
 using namespace std;
 class Queries
 {
 public:
+	Queries() { };
+	~Queries() { };
+	void ProcessQuery(PGconn* conn, const char* query);
+
 	void GetGamesByUser(string email, PGconn* conn);
 
 	vector<string> GetGamesSold(PGconn* conn);
@@ -16,8 +21,5 @@ public:
 	vector<string> GetGamesPriceRange(double priceA, double priceB, PGconn* conn);
 
 	vector<string> GetGamesByPubDev(string pub_dev, PGconn* conn);
-
-
-
 };
 
